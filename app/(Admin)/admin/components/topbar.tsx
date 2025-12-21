45837dd91edf30971f5ca94e8eff920d16d7dee4
Favurr <emekafavi2019@gmail.com>
2025-12-21 23:38:53 +0100

    Finished

--- app/(Admin)/admin/components/topbar.tsx
+++ app/(Admin)/admin/components/topbar.tsx
@@ -8,8 +8,28 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { IconLogout } from "@tabler/icons-react";
+import { toast } from "sonner";
+import { signOutUser } from "@/server/users";
+import { useRouter } from "next/navigation";
 
 export default function TopBar() {
+  const router = useRouter();
+  async function handleClick() {
+    try {
+      const response = await signOutUser();
+      if (response?.success) {
+        toast.success(response.message + ", Redirecting to login...");
+        router.push("/auth/login");
+      } else {
+        toast.error(response?.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Logout failed");
+    }
+  }
+
   return (
     <header className="flex h-14 items-center justify-end border-b px-6">
       <DropdownMenu>
@@ -22,8 +42,9 @@ export default function TopBar() {
         </DropdownMenuTrigger>
 
         <DropdownMenuContent align="end">
-          <DropdownMenuItem>
-            Logout
+          <DropdownMenuItem onClick={handleClick}>
+            <IconLogout />
+            Log Out
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
