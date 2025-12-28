3712bcd40176622a09ae2f4a7382a0da2993553a
Favurr <emekafavi2019@gmail.com>
2025-12-28 22:30:48 +0100

    Finished Support d

--- app/(Root)/blog/components/BannerGrid.tsx
+++ app/(Root)/blog/components/BannerGrid.tsx
@@ -62,7 +62,7 @@ export default function BannerGrid({ banners }: { banners: Banner[] }) {
         </div>
       </Card>
 
-      <Card className="p-6 flex flex-col items-center text-left gap-4 dark:text-black">
+      <Card className="p-6 flex flex-col justify-between text-left gap-2 dark:text-black">
         <h3 className="text-xl font-semibold">{banners[1].title}</h3>
         <p className="mt-2 text-sm text-muted-foreground dark:text-muted-foreground">
           {banners[1].subtitle}
